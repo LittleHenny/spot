a60d1f80f1c212e847dfae74b73d254b16257e7b
LittleHenny <gomanuel385@gmail.com>
2024-07-31 21:03:02 +0400

    Изменнение в конфигк и в HTML

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/spot/',
+  base: '/spot',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./spot/src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
